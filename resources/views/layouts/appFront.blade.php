2e066a27308485b5ee0e1ed4e005222bf365b182
gauthier dewit <gauthierdewit@gmail.com>
2021-06-02 16:12:23 +0200

    home.blade ok

--- resources/views/layouts/appFront.blade.php
+++ resources/views/layouts/appFront.blade.php
@@ -10,22 +10,22 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="public/img/favicon.png" rel="icon">
+  <link href="public/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="public/vendor/aos/aos.css" rel="stylesheet">
+  <link href="public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="public/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="public/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="public/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="public/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="public/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Techie - v4.1.0
@@ -47,15 +47,15 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/purecounter/purecounter.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="public/vendor/aos/aos.js"></script>
+  <script src="public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="public/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="public/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="public/vendor/php-email-form/validate.js"></script>
+  <script src="public/vendor/purecounter/purecounter.js"></script>
+  <script src="public/vendor/swiper/swiper-bundle.min.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="public/js/main.js"></script>
 
 </body>
