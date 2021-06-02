b842664e3d45f1dd506e61c8a4d0214f7aa9ab95
Your NameAntoine <martinon.toinou@protonmail.com>
2021-06-02 14:51:58 +0200

    creation des modeles et des ressources

---
+++ app/Http/Controllers/ChiffreController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chiffre;
+use Illuminate\Http\Request;
+
+class ChiffreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Chiffre  $chiffre
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chiffre $chiffre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Chiffre  $chiffre
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Chiffre $chiffre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Chiffre  $chiffre
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chiffre $chiffre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Chiffre  $chiffre
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chiffre $chiffre)
+    {
+        //
+    }
+}
