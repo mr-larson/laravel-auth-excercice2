3d76c7cc232db703ddbdee38d8a431ed70fd20d2
mr_Larson <gauthierdewit@gmail.com>
2021-06-04 07:12:20 +0200

    ajout de message d'acceuil dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,5 +33,12 @@
                     </div>
                 </a>
             </div>
+            <div class="p-10 grid md:grid-cols-1">
+                <div class="rounded overflow-hidden shadow-lg py-12 flex flex-col justify-center align-center items-center "
+                    style="background: linear-gradient(
+                            45deg, rgba(86, 58, 250, 0.9) 0%, rgba(116, 15, 214, 0.9) 100%), url('{{ asset('img/hero-bg.jpg') }}'); background-size: cover;">
+                    <p class="text-center p-2 text-xl text-white opacity-50 font-semibold">Bonjour, vous êtes bien sur la partie Admin de votre site <br> Ici vous aurez accès aux sections Counts, Service et Contact <br> Attention chaque click entraine des conséquences <br> pour tout autre renseignement contacter la Team Carambar</p>
+                </div>
+            </div>
         </div>
 @endsection
