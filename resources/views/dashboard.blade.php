a865f5048fc34c221b6daae6d6c816399cda6755
Your NameAntoine <martinon.toinou@protonmail.com>
2021-06-04 11:09:04 +0200

    slight changes on dashboard home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.appFront')
 @section('content')
     @include('layouts.navigation')
+    <div class="p-10 grid md:grid-cols-1">
+        <div class="rounded overflow-hidden shadow-lg py-12 flex flex-col justify-center align-center items-center "
+            style="background: linear-gradient(
+                    45deg, rgba(58, 250, 215, 0.9) 0%, rgba(116, 15, 214, 0.9) 100%), url('{{ asset('img/hero-bg.jpg') }}'); background-size: cover;">
+            <h2 class="text-center p-2 text-4xl text-white font-regular">Bienvenue dans l'interface admin</h2>
+            <p class="text-center p-2 text-lg text-white opacity-80 font-light">Ici vous aurez accès à la gestions de vos donnés. <br>⚠️⚠️ <u>Attention chaque click entraine des conséquences ⚠️⚠️ </u><br> <span class="pt-3 block">🍬  Pour tout autre renseignement contacter la Team Carambar🍬</span></p>
+        </div>
+    </div>
         <!--Section Counts-->
         <div class="py-12">
             <div class="p-10 grid sm:grid-cols-1 md:grid-cols-3  gap-5">
@@ -30,13 +38,6 @@
                         <p class="text-white opacity-50 font-light py-3">Gérer les chiffres</p>
                     </div>
                 </a>
-            </div>
-            <div class="p-10 grid md:grid-cols-1">
-                <div class="rounded overflow-hidden shadow-lg py-12 flex flex-col justify-center align-center items-center "
-                    style="background: linear-gradient(
-                            45deg, rgba(86, 58, 250, 0.9) 0%, rgba(116, 15, 214, 0.9) 100%), url('{{ asset('img/hero-bg.jpg') }}'); background-size: cover;">
-                    <p class="text-center p-2 text-xl text-white opacity-50 font-semibold">Bonjour, vous êtes bien sur la partie Admin de votre site <br> Ici vous aurez accès aux sections Counts, Service et Contact <br> Attention chaque click entraine des conséquences <br> pour tout autre renseignement contacter la Team Carambar</p>
-                </div>
             </div>
         </div>
 @endsection
